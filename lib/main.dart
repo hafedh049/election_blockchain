135910a0e46b9873487b176fe7918b8de2d3756c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-26 18:16:16 +0100

    Modified  lib/main.dart M  test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp
+    (
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
   }
 }
